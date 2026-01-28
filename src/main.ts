8b4f7516b4e088ef4500aa291556dface4a134ed
leanguardia <lean.guardia93@gmail.com>
2026-01-28 14:05:59 -0400

    Install swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
-import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Leancontinuo\'s Backend')
+    .setDescription('API for the Leancontinuo project')
+    .setVersion('1.0')
+    .build();
+
+  const docFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, docFactory);
+
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
